0ccfb1d8ec943c57c5cc3448bebc69ea3cfeb09b
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-09-01 08:38:55 +0100

    Key image border changes colour when corresponding key pressed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if(!audio) return;
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.add('playing');
+  if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
+  console.log(key);
 });
