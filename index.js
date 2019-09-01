2eee8f015c13d6620ace0dbabc9f10fc42797d85
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-09-01 10:24:26 +0100

    Remove the transition once ithas ended

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  key.classList.add('playing');
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-  console.log(key);
+  key.classList.add('playing');
 });
+
+function removeTransition(e){
+  // console.log(e);
+  if (e.propertyName !== 'transform') return;
+  // console.log(this);
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
